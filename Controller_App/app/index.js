03c3638aa57ac5039965e4a8c49341df403c09ce
VoTanTai-dp <thaib2111951@student.ctu.edu.vn>
2024-12-28 10:47:24 +0700

    Successfully run a command in Raspberry VM

--- Controller_App/app/index.js
+++ Controller_App/app/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { TouchableOpacity, SafeAreaView, Text, StyleSheet } from "react-native";
 
 const Home = () => {
+  const [output, setOutput] = useState("");
+
+  const handlePress = () => {
+    fetch("http://192.168.1.113:3000/run-command", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ command: 'echo "Hello World"' }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setOutput(data.output);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
         <Text style={styles.buttonText}>Start</Text>
       </TouchableOpacity>
+      <Text>{output}</Text>
     </SafeAreaView>
   );
 };
@@ -20,16 +40,16 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#6200ea",
-    borderRadius: 200, // This makes the button circular
-    width: 400, // Adjust the size as needed
-    height: 400, // Adjust the size as needed
+    borderRadius: 200,
+    width: 400,
+    height: 400,
     justifyContent: "center",
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 0.3,
     shadowRadius: 10,
-    elevation: 10, // Adds a shadow for Android
+    elevation: 10,
   },
   buttonText: {
     color: "#fff",
