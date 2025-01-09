c761d04a2caadf787327b7b2a8a2cd2fd82e4846
VoTanTai-dp <thaib2111951@student.ctu.edu.vn>
2025-01-09 14:51:18 +0700

    running expo eject

--- Controller_App/app/index.js
+++ Controller_App/app/index.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
-import { TouchableOpacity, SafeAreaView, Text, StyleSheet } from "react-native";
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import MainPage from "./components/mainPage";
 
 const Home = () => {
-  const [output, setOutput] = useState("");
-
-  const commands = ["cd ~/raspberry_pi_server", "python test.py"];
-
-  const handlePress = () => {
-    fetch("http://192.168.1.113:3000/run-command", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ commands: commands }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handling the response data
-        if (Array.isArray(data)) {
-          // Join all output/error into a single string
-          setOutput(data.map((item) => item.output || item.error).join("\n"));
-        } else {
-          setOutput(data.output || data.error);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Start</Text>
-      </TouchableOpacity>
-      <Text>{output}</Text>
+      <MainPage />
     </SafeAreaView>
   );
 };
@@ -45,24 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f5f5f5",
-  },
-  button: {
-    backgroundColor: "#6200ea",
-    borderRadius: 200,
-    width: 400,
-    height: 400,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-    elevation: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontFamily: "Lato_700Bold",
-    fontSize: 24,
   },
 });
 
